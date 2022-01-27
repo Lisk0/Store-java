afebc3374d8be19defb98d070baa98953c5173d8
Lisk0 <nemanjalisinac@gmail.com>
2022-01-27 02:06:03 +0100

    refactored Product class

--- products/Product.java
+++ products/Product.java
@@ -4,5 +4,12 @@ public class Product {
 	protected String name;
 	protected String brand;
 	protected float price;
+	protected float discount;
+	
+	public Product(String name, String brand, float price) {
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+	}
 	
 }
