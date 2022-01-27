512da61e27e32ca846fd00a348aa698e057d353f
Lisk0 <nemanjalisinac@gmail.com>
2022-01-27 02:29:13 +0100

    added appliance class

--- products/Appliance.java
+++ products/Appliance.java
@@ -1,5 +1,31 @@
 package products;
 
-public class Appliance {
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
+public class Appliance extends Product{
+	
+	private String model;
+	private LocalDate productionDate;
+	private float weight;
+	
+	public Appliance(String name, String brand, float price, String model, LocalDate productionDate, float weight) {
+		super(name, brand, price);
+		this.model = model;
+		this.productionDate = productionDate;
+		this.weight = weight;
+	}
+	
+	public float getDiscount() {
+		float discount;
+		LocalDate current= LocalDate.now();
+		DayOfWeek d= DayOfWeek.from(current);
+		int value= d.getValue();
+		if(price>999 && value>5) {
+			discount=0.05f;
+		}
+		else discount=0.0f;
+		return discount;
+	}
+	
 }
